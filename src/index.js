77a0c5d8445bb2179d77b27f4addff7942b1cdee
Richie <molunorichie@gmail.com>
2022-10-05 00:48:31 +0100

    add: db connection - index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,20 @@
 import app from "./app.js";
 
-import { PORT } from "./utils/config.js";
+import { PORT, dbUri } from "./utils/config.js";
+import { connectDB } from "./db/connect_db.js";
 
-app.listen(PORT, () => console.log(`server running on PORT ${PORT}`));
+async function start() {
+    try {
+        // Connect to Database
+        await connectDB(dbUri);
+
+        app.listen(PORT, function () {
+            console.log(`Server is running on port ${PORT}....`);
+        });
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+start()
+
